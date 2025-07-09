a52ea6d1ff69da714453434badaa65b0b40ed545
MR.SAMIR <samirkumarpanjha@gmail.com>
2025-07-09 08:22:19 +0530

    intial commit

---
+++ src/main/java/com/smr/service/App.java
@@ -0,0 +1,22 @@
+package com.smr.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public int sum(int x, int y) {
+		return x+y;
+	}
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        App app = new App();
+        System.out.println("sum is ::" + app.sum(10, 20));
+    }
+}
+
+
+
+C:\Users\HP\.m2\repository\smr\MavenProjo2-StringUtil\0.0.1-SNAPSHOT
